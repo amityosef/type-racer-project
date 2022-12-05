01e0dd587aab3bab719e106c8fec641d1e46e971
amityosef <amityosef28@email.com>
2022-12-05 11:13:59 +0200

    fff

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import 'core-js/features/array/flat-map'
-import 'core-js/features/map'
-import 'core-js/features/promise'
-import 'core-js/features/set'
-import 'raf/polyfill'
-import 'whatwg-fetch'
+import "core-js/features/array/flat-map";
+import "core-js/features/map";
+import "core-js/features/promise";
+import "core-js/features/set";
+import "raf/polyfill";
+import "whatwg-fetch";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"
+import "./index.css";
+import { HomePage } from "./pages/homePage/homePage";
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
-root.render(<h1>Hello React!</h1>);
+root.render(<HomePage />);
